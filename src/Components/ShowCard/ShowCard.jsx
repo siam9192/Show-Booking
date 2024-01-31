52d4371e3d49649dbde755d508edb5f56422b616
siam9192 <ahsiam999@gmail.com>
2024-02-01 03:29:30 +0600

    Show details page added

--- src/Components/ShowCard/ShowCard.jsx
+++ src/Components/ShowCard/ShowCard.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import { GoArrowUpRight } from "react-icons/go";
+import { HiStar } from "react-icons/hi";
+import { useNavigate } from 'react-router-dom';
 const ShowCard = ({show}) => {
+    const navigate = useNavigate();
+    
+    const handleNavigate = (id)=>{
+        navigate(`/show/details/${id}`)
+    }
     return (
         <div className='show-card'>
            <div className='show-card-image-container'>
            <img src={show.show.image?.original||show.show.image?.medium}   />
+         {
+            show.show.rating.average &&   <div className='show-card-ratting'><HiStar className='show-card-ratting-icon'></HiStar><h4>{show.show.rating.average||0}</h4></div>
+         }
            </div>
             <div className='show-card-text-container'>
                 <h3>{show.show.name}</h3>
-                <div className='show-cart-extra-info'>
+                <div className='details-extra-info'>
                     <div className='show-type'>{show.show.type}</div>
                     <div className='show-language'>{show.show.language}</div>
                 </div>
                 <div className='show-card-button-container'>
-                    <button><GoArrowUpRight></GoArrowUpRight></button>
+                    <div className='show-card-status'>{''}</div>
+                    <button><GoArrowUpRight onClick={()=>handleNavigate(show.show.id)}></GoArrowUpRight></button>
                 </div>
             </div>
         </div>
--- src/Pages/ShowDetails/ShowDetails.jsx
+++ src/Pages/ShowDetails/ShowDetails.jsx
@@ -1,8 +1,51 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useRef, useState } from 'react';
+import './ShowDetails.css'
+import { useParams } from 'react-router-dom';
+import { BsClockHistory } from "react-icons/bs";
 
 const ShowDetails = () => {
+    const [show,setShow] = useState({})
+    const {id} = useParams();
+    const summaryRef = useRef();
+  
+    useEffect(()=>{
+        axios.get('https://api.tvmaze.com/search/shows?q=all')
+        .then(res =>{
+            const data = res.data;
+            const findShow = data.find(item=> item.show.id === parseInt(id))
+            setShow(findShow)
+            summaryRef.current.innerHTML = `${findShow.show.summary}`
+        })
+    })
     return (
-        <div>
+        <div className='details-main-container'>
+            <div className='details-container'>
+              <div className='image-container'>
+              <img src={show?.show?.image.original} alt="" />
+              <button className='book-ticket-btn'>Book Ticket</button>
+              </div>
+
+                <div className='main-details'>
+                    <h1>{show?.show?.name}</h1>
+                    <div className='details-extra-info'>
+                    <div className='show-type'>{show?.show?.type}</div>
+                    <div className='show-language'>{show?.show?.language}</div>
+                </div>
+                    <p ref={summaryRef}>{show?.show?.summary}</p>
+                    <div >
+                  <div style={{display:'flex',justifyItems:'center',gap:'12px'}}>
+                  <div style={{color:'#dd003f', fontSize:'20px'}}><BsClockHistory></BsClockHistory>  </div> <h4>Available Schedule</h4>
+                  </div>
+                        <div className='schedule-container'>
+                            {
+                                show?.show?.schedule.days.map((item,index)=> <div className='schedule'>{item}</div>)
+                            }
+                        </div>
+                        <h4 className='schedule-time'><span>Time</span> : {show?.show?.schedule.time}</h4>
+                    </div>
+                </div>
+            </div>
             
         </div>
     );
